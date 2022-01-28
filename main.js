d1b4b478e9498db8b486a910c9df9d0528d79b9f
ezekielswanson <swansonezekiel@gmail.com>
2022-01-27 19:30:47 -0600

    Updated files and added js to navigation.

---
+++ main.js
@@ -0,0 +1,10 @@
+/* SECTION VARIABLES */
+
+const menuDisplayText = document.querySelector('.inactive');
+const closeDisplayText = document.querySelector('.active');
+const hamburgerMenuBtn = document.querySelector('.menu-btn');
+
+/* The text close is hidden on page load */
+
+closeDisplayText.style.display = 'none';
+
